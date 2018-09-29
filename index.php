151cc4f4d7712102791a9fffdff0c76392968ff3
allejo392 <alejo_chiqui@hotmail.com>
2018-09-29 13:07:36 -0500

    Add files via upload
    updated

---
+++ index.php
@@ -0,0 +1,268 @@
+    <?php get_header(); ?>
+    <main class="clear">
+      <div class="video-bubble hero halfway-section">
+        <div class="compartment">
+          <div class="video__details">
+            <h1 class="larger">Capture a Stronger Supply Chain.</h1>
+            <p>From basic barcode labels to enterprise-wide solutions, our team takes the time to learn about your unique business needs. Then we develop, deploy and manage cost-effective solutions that let you sleep better at night and <strong>make your customers smile.</strong></p><a href="" class="button button--inverse full-width">Ask a Question</a><a href="" class="button button--inverse full-width">Request a Quote</a><a href="" class="button button--inverse full-width">Shop Now</a>
+          </div>
+        </div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/hero.jpg" alt="undefined">
+      </div>
+      <ul class="swift-slide product-slide halfway-section">
+        <li class="hero product">
+          <div class="compartment">
+            <div class="product__details">
+              <h2 class="larger">Slide 1</h2>
+              <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo.</p><a href="" class="button">Read More</a>
+            </div>
+          </div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/honeywell.jpg" alt="undefined">
+        </li>
+        <li class="hero product">
+          <div class="compartment">
+            <div class="product__details">
+              <h2 class="larger">Slide 2</h2>
+              <p>Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p><a href="" class="button">Read More</a>
+            </div>
+          </div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/honeywell.jpg" alt="undefined">
+        </li>
+        <li class="hero product">
+          <div class="compartment">
+            <div class="product__details">
+              <h2 class="larger">Slide 3</h2>
+              <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id nibh ultricies vehicula ut id elit.</p><a href="" class="button">Read More</a>
+            </div>
+          </div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/honeywell.jpg" alt="undefined">
+        </li>
+      </ul>
+      <div class="halfway-section">
+        <ul class="partners">
+          <li class="partners__title">
+            <h2 class="stay">Our Trusted Partners</h2>
+          </li>
+          <li class="partners__item"><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/descartes.png" alt="undefined"></a></li>
+          <li class="partners__item"><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/honeywell.png" alt="undefined"></a></li>
+          <li class="partners__item"><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/nicelabel.png" alt="undefined"></a></li>
+          <li class="partners__item"><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/intellitrack.png" alt="undefined"></a></li>
+          <li class="partners__item"><a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/zebra.png" alt="undefined"></a></li>
+        </ul>
+        <div class="bubble bubble--big bubble--white">
+          <div class="compartment">
+            <div class="blockquote-bubble clear">
+                <figure class="shifted-asset shifted-asset--left"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/big-circle.jpg" alt="">
+                  <figcaption></figcaption>
+                </figure>
+              <div class="block-out">
+                <blockquote>“Whether it’s modifying existing software or buying new hardware, the <strong>team at Barcoding, Inc. has my best interest at heart</strong>. They’ve learned my business and they’re willing to help me however they can.”
+                  <cite>DONNA BECRAFT, CIO, Dunbar Armored</cite>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="grid halfway-section">
+        <div class="col-aldnoah-6">
+          <div class="callout callout--blue callout--larger hero"><a href="" class="callout__details">
+              <h3 class="mini">Blog</h3>
+              <h2 class="larger">Walmart Using Drones in Quest to Outperform Amazon</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt urna vitae mauris rutrum interdum. Suspendisse eu lectus a leo tempor rhoncus. Cras lobortis vitae orci vitae finibus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt urna vitae mauris rutrum interdum. Suspendisse eu lectus a leo tempor rhoncus. Cras lobortis vitae orci vitae finibus</p></a>
+              <div class="callout__share">
+                <h3 class="mini"><span>Share +</span></h3>
+                <nav class="nav nav-social">
+                  <ul>
+                    <li><a href="" target="_blank"><span class="bottle">
+                            <svg class="symbol symbol-pinterest">
+                              <use xlink:href="#pinterest"></use>
+                            </svg></span><span class="visible-for-screen-readers">Pinterest</span></a></li>
+                    <li><a href="" target="_blank"><span class="bottle">
+                            <svg class="symbol symbol-google">
+                              <use xlink:href="#google"></use>
+                            </svg></span><span class="visible-for-screen-readers">Google+</span></a></li>
+                    <li><a href="" target="_blank"><span class="bottle">
+                            <svg class="symbol symbol-linkedin">
+                              <use xlink:href="#linkedin"></use>
+                            </svg></span><span class="visible-for-screen-readers">linkedin</span></a></li>
+                    <li><a href="" target="_blank"> <span class="bottle">
+                            <svg class="symbol symbol-twitter">
+                              <use xlink:href="#twitter"></use>
+                            </svg></span><span class="visible-for-screen-readers">Twitter   </span></a></li>
+                    <li><a href="" target="_blank"><span class="bottle">
+                            <svg class="symbol symbol-facebook">
+                              <use xlink:href="#facebook"></use>
+                            </svg></span><span class="visible-for-screen-readers">Facebook</span></a></li>
+                  </ul>
+                </nav>
+              </div><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/hero.jpg" alt="undefined">
+          </div>
+        </div>
+        <div class="col-aldnoah-6">
+          <div class="grid">
+            <div class="col-medium-6">
+              <div class="callout callout--orange"><a href="" class="callout__details">
+                  <h3 class="mini">Whitepapers</h3>
+                  <h2 class="larger">Asset, Patient Tracking Technology Jockeys for Bottom-line Position.</h2></a>
+                  <div class="callout__share">
+                    <h3 class="mini"><span>Share +</span></h3>
+                    <nav class="nav nav-social">
+                      <ul>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-pinterest">
+                                  <use xlink:href="#pinterest"></use>
+                                </svg></span><span class="visible-for-screen-readers">Pinterest</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-google">
+                                  <use xlink:href="#google"></use>
+                                </svg></span><span class="visible-for-screen-readers">Google+</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-linkedin">
+                                  <use xlink:href="#linkedin"></use>
+                                </svg></span><span class="visible-for-screen-readers">linkedin</span></a></li>
+                        <li><a href="" target="_blank"> <span class="bottle">
+                                <svg class="symbol symbol-twitter">
+                                  <use xlink:href="#twitter"></use>
+                                </svg></span><span class="visible-for-screen-readers">Twitter   </span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-facebook">
+                                  <use xlink:href="#facebook"></use>
+                                </svg></span><span class="visible-for-screen-readers">Facebook</span></a></li>
+                      </ul>
+                    </nav>
+                  </div>
+              </div>
+            </div>
+            <div class="col-medium-6">
+              <div class="callout callout--gray"><a href="" class="callout__details">
+                  <h3 class="mini">Events</h3>
+                  <h2 class="larger">Annual Barcoding, Inc. Executive Forum</h2>
+                  <h2 class="larger">9.12.16</h2></a>
+                  <div class="callout__share">
+                    <h3 class="mini"><span>Share +</span></h3>
+                    <nav class="nav nav-social">
+                      <ul>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-pinterest">
+                                  <use xlink:href="#pinterest"></use>
+                                </svg></span><span class="visible-for-screen-readers">Pinterest</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-google">
+                                  <use xlink:href="#google"></use>
+                                </svg></span><span class="visible-for-screen-readers">Google+</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-linkedin">
+                                  <use xlink:href="#linkedin"></use>
+                                </svg></span><span class="visible-for-screen-readers">linkedin</span></a></li>
+                        <li><a href="" target="_blank"> <span class="bottle">
+                                <svg class="symbol symbol-twitter">
+                                  <use xlink:href="#twitter"></use>
+                                </svg></span><span class="visible-for-screen-readers">Twitter   </span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-facebook">
+                                  <use xlink:href="#facebook"></use>
+                                </svg></span><span class="visible-for-screen-readers">Facebook</span></a></li>
+                      </ul>
+                    </nav>
+                  </div>
+              </div>
+            </div>
+            <div class="col-flush">
+              <div class="callout callout--dark-blue callout--featured"><a href="" class="callout__details">
+                  <h3 class="mini">Featured Partner</h3>
+                  <div class="callout__logo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/honeywell-white.png" alt="undefined">
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tincidunt urna vitae mauris rutrum interdum. Suspendisse eu lectus a leo tempor rhoncus. Cras lobortis vitae orci vitae finibus</p></a>
+                  <div class="callout__share">
+                    <h3 class="mini"><span>Share +</span></h3>
+                    <nav class="nav nav-social">
+                      <ul>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-pinterest">
+                                  <use xlink:href="#pinterest"></use>
+                                </svg></span><span class="visible-for-screen-readers">Pinterest</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-google">
+                                  <use xlink:href="#google"></use>
+                                </svg></span><span class="visible-for-screen-readers">Google+</span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-linkedin">
+                                  <use xlink:href="#linkedin"></use>
+                                </svg></span><span class="visible-for-screen-readers">linkedin</span></a></li>
+                        <li><a href="" target="_blank"> <span class="bottle">
+                                <svg class="symbol symbol-twitter">
+                                  <use xlink:href="#twitter"></use>
+                                </svg></span><span class="visible-for-screen-readers">Twitter   </span></a></li>
+                        <li><a href="" target="_blank"><span class="bottle">
+                                <svg class="symbol symbol-facebook">
+                                  <use xlink:href="#facebook"></use>
+                                </svg></span><span class="visible-for-screen-readers">Facebook</span></a></li>
+                      </ul>
+                    </nav>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="halfway-section">
+        <div class="social-feeds">
+          <div class="compartment">
+            <ul>
+              <li><a href="" target="_blank" class="social__bubble social__bubble--facebook">
+                  <div class="social__icon">
+                      <svg class="symbol symbol-facebook">
+                        <use xlink:href="#facebook"></use>
+                      </svg>
+                  </div></a>
+                <p>Could #‎hyperloop‬ systems replace trucks and rail for cargo <strong>‪#‎transportation‬</strong>?</p>
+              </li>
+              <li><a href="" target="_blank" class="social__bubble social__bubble--twitter">
+                  <div class="social__icon">
+                      <svg class="symbol symbol-twitter">
+                        <use xlink:href="#twitter"></use>
+                      </svg>
+                  </div></a>
+                <p>Proud sponsor of World Refugee Day in <strong>#Baltimore @RYPnews #CelebrateDiversity @ShaneBarcode</strong></p>
+              </li>
+              <li><a href="" target="_blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/geeknetwork.png" class="full-width"></a></li>
+            </ul>
+          </div>
+        </div>
+        <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+          <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="post-header">
+               <div class="date"><?php the_time( 'M j y' ); ?></div>
+               <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+               <div class="author"><?php the_author(); ?></div>
+            </div><!--end post header-->
+            <div class="entry clear">
+               <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+               <?php the_content(); ?>
+               <?php edit_post_link(); ?>
+               <?php wp_link_pages(); ?> </div>
+            <!--end entry-->
+            <div class="post-footer">
+               <div class="comments"><?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
+            </div><!--end post footer-->
+            </div><!--end post-->
+        <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
+            <div class="navigation index">
+               <div class="alignleft"><?php next_posts_link( 'Older Entries' ); ?></div>
+               <div class="alignright"><?php previous_posts_link( 'Newer Entries' ); ?></div>
+            </div><!--end navigation-->
+        <?php else : ?>
+        <?php endif; ?>
+
+        <div class="sign-up">
+          <div class="compartment">
+            <div class="sign-up__bubble">
+              <h2>Sign up for our newsletter:</h2>
+              <div class="sign-up__form">
+                <input type="text" placeholder="Email Address">
+                <input type="submit" value="Sign Up" class="button">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <?php get_footer(); ?>
